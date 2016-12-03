3d7d76afc7b85f6bd78022ec853697247f66225c
Douglas Gordon <douglastgordon@gmail.com>
2016-12-03 09:59:01 -0500

    finish d3_p2

--- d3_p1.rb
+++ d3_p1.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 triangle_count = 0
 
 
---
+++ d3_p2.rb
@@ -0,0 +1,30 @@
+require 'byebug'
+triangle_count = 0
+
+def real_triangle?(arr)
+  sides_total = arr.inject(:+)
+  max = arr.max
+  max < (sides_total - max)
+end
+
+triangles = []
+
+File.open('d3_p1_input.txt').each do |triangle|
+  triangle = triangle.split.map{|el| el.to_i}
+  triangles << triangle
+end
+
+triangles = triangles.transpose
+triangles.each do |row|
+  i = 0
+  while i < row.length - 2
+    if real_triangle?([ row[i], row[i+1], row[i+2] ])
+      triangle_count += 1
+    end
+    i += 3
+  end
+end
+
+
+
+p triangle_count
