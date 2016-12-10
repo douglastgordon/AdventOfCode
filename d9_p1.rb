9aa73eae268e06a5efcb5d10c887ad6aeddc3844
Douglas Gordon <douglastgordon@gmail.com>
2016-12-10 12:16:36 -0500

    finish d9 p1

--- d9_p1.rb
+++ d9_p1.rb
@@ -23,7 +23,6 @@ def instructions?(message)
   message.length == 2
 end
 
-
 def parse_input
   input = File.open('d9_input.txt').read
   input = input.split("(")
@@ -46,4 +45,30 @@ def run
   decrypted_message.length
 end
 
-p run
+
+def decrypt2
+  input = File.open('d9_input.txt').read.split("")
+  i = 0
+  output = ""
+  while i < input.length
+    if input[i] == "("
+      instructions = "("
+      until input[i] == ")"
+        i += 1
+        instructions << input[i]
+      end
+      instructions = instructions[1..-2].split("x").map{|el| el.to_i}
+      instructions[1].times do
+        output << input[(i+1)..(i+instructions[0])].join("")
+      end
+      i += instructions[0] + 1
+    else
+      output << input[i]
+      i += 1
+    end
+  end
+  p output
+  output.chomp.length
+end
+
+p decrypt2
